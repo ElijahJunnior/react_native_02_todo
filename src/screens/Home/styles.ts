3e0fcc21a6e12255f57a7388a8041b9b601aebcc
Elias Junior <elijah.junnior@outlook.com>
2024-06-17 23:24:30 -0300

    organização inicial do projeto

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+
+// cores base 
+// blue-dark    #1E6F9F
+// blue         #4EA8DE
+// purple-dark  #5E60CE
+// purple       #8284FA
+
+// cores produto
+// gary-700     #0D0D0D
+
+
+export const styles = StyleSheet.create({
+
+})
